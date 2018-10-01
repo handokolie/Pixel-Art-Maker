ec830671f9069c44bcc2bda2f32f08c8484eddba
Handoko Lie <2717558+handokolie@users.noreply.github.com>
2018-10-01 09:36:54 +0700

    Update designs.js -- Change var and let to const
    Change all var and let to const

--- designs.js
+++ designs.js
@@ -2,14 +2,14 @@
 * that the user can interact with.*/
 function makeGrid() {
   // Assign the height and width of the canvas as variables.
-  let height = document.querySelector('#inputHeight').value;
-  let width = document.querySelector('input#inputWidth').value;
+  const height = document.querySelector('#inputHeight').value;
+  const width = document.querySelector('input#inputWidth').value;
   // Assign the canvas DOM as a variable table.
   const table = document.querySelector('#pixelCanvas');
   // Refresh the table when the makeGrid function is called.
   table.innerHTML = '';
   // Assign the value of the colorPicker as a variable color.
-  let color = document.querySelector('#colorPicker').value;
+  const color = document.querySelector('#colorPicker').value;
   /** Use a nested for loop to construct the canvas
   * and attach an event listener in each cell of the canvas.*/
   for (var r = 0; r < height; r += 1) {
@@ -30,7 +30,7 @@ function makeGrid() {
 }
 
 // When the submit button is clicked by the user, call makeGrid()
-var submit = document.querySelector('input[type="submit"]');
+const submit = document.querySelector('input[type="submit"]');
 
 submit.addEventListener('click', function (event) {
   event.preventDefault();
